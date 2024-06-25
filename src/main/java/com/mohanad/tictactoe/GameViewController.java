6c7fc27edc1bd76832db33584754407edd58bd76
Mohanad <mohanadkhaled87@gmail.com>
2024-06-25 16:04:44 +0300

    finished game logic and added home/rematch after the game ends

--- src/main/java/com/mohanad/tictactoe/GameViewController.java
+++ src/main/java/com/mohanad/tictactoe/GameViewController.java
@@ -3,14 +3,19 @@ package com.mohanad.tictactoe;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class GameViewController {
     @FXML
@@ -24,14 +29,11 @@ public class GameViewController {
 
     @FXML
     public void initialize() {
-        // Define the number of rows and columns
         int numRows = 3;
         int numCols = 3;
 
-        // Clear any existing content
         gridPane.getChildren().clear();
 
-        // Dynamically create and add Pane elements with borders to the GridPane
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
                 Pane pane = createCellPane(row, col);
@@ -41,6 +43,7 @@ public class GameViewController {
         }
         playerNames[0] = PlayersDetails.username1;
         playerNames[1] = PlayersDetails.username2;
+
         currPlayerTurn.setText(playerNames[0] + "'s turn!");
     }
 
@@ -70,6 +73,11 @@ public class GameViewController {
 
         grid[row][col] = currentPlayerMove;
         addNewLabelToPane(event);
+        GameStatus currGameStatus = getGameStatus();
+        if(!currGameStatus.equals(GameStatus.Running)){
+            handleEndOfGame(currGameStatus,event);
+            return;
+        }
         if(currentPlayerMove == 'X') {
             currentPlayerMove = 'O';
             currPlayerTurn.setText(playerNames[1] + "'s turn!");
@@ -79,6 +87,36 @@ public class GameViewController {
             currPlayerTurn.setText(playerNames[0] + "'s turn!");
         }
     }
+
+    private void handleEndOfGame(GameStatus currGameStatus,Event event){
+        Alert alert = new Alert(Alert.AlertType.NONE);
+        ButtonType homeButton = new ButtonType("Home");
+        ButtonType rematchButton = new ButtonType("Rematch");
+        alert.getButtonTypes().addAll(homeButton,rematchButton);
+        alert.setContentText(currGameStatus.label);
+        alert.showAndWait().ifPresent(buttonType -> {
+            if(buttonType.getText().equals(homeButton.getText())){
+                try {
+                    FXHelpers.closeStageFromEvent(event);
+                    FXHelpers.loadNewStage("hello-view.fxml");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else{
+                FXHelpers.closeStageFromEvent(event);
+                try {
+                    FXHelpers.loadNewStage("game-view.fxml");
+                    FXHelpers.closeStageFromEvent(event);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+        return;
+    }
+
     private void addNewLabelToPane(Event event){
         Label label = new Label(String.valueOf(currentPlayerMove));
         label.setStyle("-fx-font-size: 36; -fx-text-fill: " + (currentPlayerMove == 'X' ? "red;" : "blue;") +
@@ -89,8 +127,7 @@ public class GameViewController {
     }
 
 
-    private static GameStatus checkIfGameOver() {
-        // Check rows
+    private static GameStatus getGameStatus() {
         for (int row = 0; row < 3; row++) {
             if (grid[row][0] == grid[row][1] && grid[row][1] == grid[row][2] && grid[row][0] != '.') {
                 if (grid[row][0] == 'X') {
@@ -101,7 +138,6 @@ public class GameViewController {
             }
         }
 
-        // Check columns
         for (int col = 0; col < 3; col++) {
             if (grid[0][col] == grid[1][col] && grid[1][col] == grid[2][col] && grid[0][col] != '.') {
                 if (grid[0][col] == 'X') {
@@ -112,7 +148,6 @@ public class GameViewController {
             }
         }
 
-        // Check diagonals
         if ((grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != '.') ||
                 (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] != '.')) {
             if (grid[1][1] == 'X') {
@@ -122,7 +157,6 @@ public class GameViewController {
             }
         }
 
-        // Check for draw
         boolean draw = true;
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
@@ -142,7 +176,15 @@ public class GameViewController {
     }
 
     enum GameStatus {
-        Player1Won, Player2Won, Draw,Running;
+        Player1Won("Player 1 has won!"),
+        Player2Won("Player 2 has won!"),
+        Draw("Draw!"),
+        Running("Running!");
+
+        public final String label;
+        GameStatus(String label) {
+            this.label = label;
+        }
     }
 
 
