03d1f547e38cd1696b04053361788b804a7b2b28
Mohanad <mohanadkhaled87@gmail.com>
2024-06-25 16:15:33 +0300

    fixed the fxmlFileName variable

--- src/main/java/com/mohanad/tictactoe/FXHelpers.java
+++ src/main/java/com/mohanad/tictactoe/FXHelpers.java
@@ -15,7 +15,7 @@ public class FXHelpers {
         currStage.close();
     }
     public static void loadNewStage(String fxmlFileName) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFileName));
         Scene scene = new Scene(fxmlLoader.load(), 728, 484);
         Stage stage = new Stage();
         stage.resizableProperty().setValue(Boolean.FALSE);
